c324ecd5cc1a3d60352e3439be32f81b284e0849
Diana Gudu <diana.gudu@kit.edu>
2021-05-12 18:58:52 +0200

    log gunicorn to stderr instead of file

--- runner.sh
+++ runner.sh
@@ -4,6 +4,9 @@ export $(grep -v '^#' /etc/motley_cue/motley_cue.env | xargs)
 env
 
 service nginx start &
-/usr/lib/motley-cue/bin/gunicorn motley_cue.api:api -k "uvicorn.workers.UvicornWorker" --config /etc/motley_cue/gunicorn.conf.py &
+/usr/lib/motley-cue/bin/gunicorn motley_cue.api:api \
+    -k "uvicorn.workers.UvicornWorker" \
+    --config /etc/motley_cue/gunicorn.conf.py \
+    --access-logfile "-" --error-logfile "-" &
 /usr/sbin/sshd -D -e
 wait -n
